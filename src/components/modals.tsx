02b83ac64a1d3dbf2f6c035ca0b57ab7ad79f396
Surya Ashish <ashishsurya126@gmail.com>
2023-04-05 00:25:07 +0530

    refactored code in modals.tsx

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { Transition, Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useCallback, useState } from "react";
 import { useRecoilState } from "recoil";
 import { createDeckModalAtom } from "~/atoms/createDeckModalAtom";
@@ -13,7 +13,7 @@ export const CreateNewDeck = () => {
   const [newDeckModalState, setNewDeckModalState] =
     useRecoilState(createDeckModalAtom);
 
-  const closeModal = useCallback(
+  const onClose = useCallback(
     () => setNewDeckModalState({ open: false }),
     [setNewDeckModalState]
   );
@@ -22,11 +22,66 @@ export const CreateNewDeck = () => {
     await mutateAsync({ title: input });
     void ctx.deck.getDecks.invalidate();
     setNewDeckModalState({ open: false });
-  }, [ctx.deck.getDecks, input, mutateAsync]);
+  }, [ctx.deck.getDecks, input, mutateAsync, setNewDeckModalState]);
 
   return (
-    <Transition appear show={newDeckModalState.open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+    <ModalLayout show={newDeckModalState.open} onClose={onClose}>
+      <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-slate-800 p-6 text-left align-middle text-white shadow-xl transition-all">
+        <Dialog.Title
+          as="h2"
+          className={"text-center text-2xl font-bold tracking-tighter"}
+        >
+          Create a New Deck
+        </Dialog.Title>
+        <Dialog.Description
+          as="div"
+          className="flex w-full flex-col items-center space-y-4 pt-10"
+        >
+          <div className="w-full self-start">
+            <label htmlFor="new-deck-input">
+              <p className="pb-2">Deck Title : </p>
+            </label>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              className="w-full rounded-md border border-slate-400 bg-transparent px-4  py-2 focus:outline-none"
+              placeholder="Enter title of new deck"
+              id="new-deck-input"
+            />
+          </div>
+          <div className="self-end">
+            <button
+              onClick={onClose}
+              className="bg-transparent text-red-400 hover:shadow-none"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={createnewDeck}
+              className="!bg-black  bg-transparent  text-center hover:shadow-none"
+            >
+              {isPosting ? <LoadingSpinner /> : "Create"}
+            </button>
+          </div>
+        </Dialog.Description>
+      </Dialog.Panel>
+    </ModalLayout>
+  );
+};
+
+export const ModalLayout = ({
+  children,
+  show,
+  onClose,
+}: {
+  children: React.ReactNode;
+  show: boolean;
+  onClose: () => void;
+}) => {
+  return (
+    <Transition appear show={show} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -50,46 +105,7 @@ export const CreateNewDeck = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-slate-800 p-6 text-left align-middle text-white shadow-xl transition-all">
-                <Dialog.Title
-                  as="h2"
-                  className={"text-center text-2xl font-bold tracking-tighter"}
-                >
-                  Create a New Deck
-                </Dialog.Title>
-                <Dialog.Description
-                  as="div"
-                  className="flex w-full flex-col items-center space-y-4 pt-10"
-                >
-                  <div className="w-full self-start">
-                    <label htmlFor="new-deck-input">
-                      <p className="pb-2">Deck Title : </p>
-                    </label>
-                    <input
-                      value={input}
-                      onChange={(e) => setInput(e.target.value)}
-                      type="text"
-                      className="w-full rounded-md border border-slate-400 bg-transparent px-4  py-2 focus:outline-none"
-                      placeholder="Enter title of new deck"
-                      id="new-deck-input"
-                    />
-                  </div>
-                  <div className="self-end">
-                    <button
-                      onClick={closeModal}
-                      className="bg-transparent text-red-400 hover:shadow-none"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      onClick={createnewDeck}
-                      className="!bg-black  bg-transparent  text-center hover:shadow-none"
-                    >
-                      {isPosting ? <LoadingSpinner /> : "Create"}
-                    </button>
-                  </div>
-                </Dialog.Description>
-              </Dialog.Panel>
+              {children}
             </Transition.Child>
           </div>
         </div>
